b454fdd2a90f1a1d734160344e895f789924cc6e
gaeunnlee <gaeunnnleee@gmail.com>
2026-03-27 03:06:30 +0900

    feat(news): add news collection infrastructure (#19)

---
+++ src/main/java/com/solv/wefin/domain/news/config/NewsConfig.java
@@ -0,0 +1,14 @@
+package com.solv.wefin.domain.news.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class NewsConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
