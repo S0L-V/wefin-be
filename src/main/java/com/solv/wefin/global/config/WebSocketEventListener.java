9ce9c0ac4f8882dd84eb68ad1d2ceadd66b94a8e
김민서 <minseokim0113@gmail.com>
2026-03-28 12:11:34 +0900

    feat(chat): add stomp interceptor and websocket event listener (#49)

---
+++ src/main/java/com/solv/wefin/global/config/WebSocketEventListener.java
@@ -0,0 +1,39 @@
+package com.solv.wefin.global.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Slf4j
+@Component
+public class WebSocketEventListener {
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectEvent event) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        Object user = accessor.getUser();
+
+        if (user instanceof Authentication auth) {
+            log.info("WebSocket connected user={}", auth.getName());
+        } else {
+            log.info("WebSocket connected");
+        }
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        Object user = accessor.getUser();
+
+        if (user instanceof Authentication auth) {
+            log.info("WebSocket disconnected user={}", auth.getName());
+        } else {
+            log.info("WebSocket disconnected");
+        }
+    }
+}
