8b7f310dd20dd97a35c749ecbe986f17a920f067
김민서 <minseokim0113@gmail.com>
2026-03-30 01:00:05 +0900

    feat(global-chat): add global chat controller and query api for recent messages (#41)

---
+++ src/main/java/com/solv/wefin/web/chat/globalChat/GlobalChatQueryController.java
@@ -0,0 +1,23 @@
+package com.solv.wefin.web.chat.globalChat;
+
+import com.solv.wefin.domain.chat.globalChat.service.GlobalChatService;
+import com.solv.wefin.web.chat.globalChat.dto.response.GlobalChatMessageResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chat/global")
+public class GlobalChatQueryController {
+
+    private final GlobalChatService globalChatService;
+
+    @GetMapping("/messages")
+    public List<GlobalChatMessageResponse> getRecentMessage() {
+        return globalChatService.getRecentMessages();
+    }
+}
