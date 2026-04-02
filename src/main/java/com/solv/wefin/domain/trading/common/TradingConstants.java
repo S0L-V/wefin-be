720b80a598360dcd3eaa630e303e7be9b6412e2f
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-04-02 21:55:54 +0900

    extract TradingConstants for FEE_RATE and TAX_RATE (#107)

---
+++ src/main/java/com/solv/wefin/domain/trading/common/TradingConstants.java
@@ -0,0 +1,8 @@
+package com.solv.wefin.domain.trading.common;
+
+import java.math.BigDecimal;
+
+public class TradingConstants {
+	public static final BigDecimal FEE_RATE = new BigDecimal("0.00015");
+	public static final BigDecimal TAX_RATE = new BigDecimal("0.0018");
+}
