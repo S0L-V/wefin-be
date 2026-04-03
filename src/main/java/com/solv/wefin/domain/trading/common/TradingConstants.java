4add9cb25b0aae565758ffd97729dd55f642199b
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-04-03 17:28:48 +0900

    refactor(trading): add private constructor to TradingConstants (#107)

--- src/main/java/com/solv/wefin/domain/trading/common/TradingConstants.java
+++ src/main/java/com/solv/wefin/domain/trading/common/TradingConstants.java
@@ -5,4 +5,7 @@ import java.math.BigDecimal;
 public class TradingConstants {
 	public static final BigDecimal FEE_RATE = new BigDecimal("0.00015");
 	public static final BigDecimal TAX_RATE = new BigDecimal("0.0018");
+
+	private TradingConstants() {
+	}
 }
