d7fafd27a76c1443811b2378fe843a10ba709987
gaeunnlee <gaeunnnleee@gmail.com>
2026-04-04 15:58:54 +0900

    feat(chat): add reply_to_message_id to chat_message (#112)

--- src/main/resources/db/migration/V17__fix_published_at_timestamptz.sql
+++ src/main/resources/db/migration/V17__fix_published_at_timestamptz.sql
@@ -4,3 +4,10 @@ ALTER TABLE news_article
 
 ALTER TABLE raw_news_article
     ALTER COLUMN original_published_at TYPE TIMESTAMPTZ;
+
+ALTER TABLE chat_message
+    ADD COLUMN reply_to_message_id BIGINT NULL;
+
+ALTER TABLE chat_message
+    ADD CONSTRAINT fk_chat_message_reply_to
+        FOREIGN KEY (reply_to_message_id) REFERENCES chat_message(message_id);
