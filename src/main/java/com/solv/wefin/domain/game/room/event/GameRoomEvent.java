ae40254cae4f7bcfb36fd820aeb5a5c81f28d6f2
hjh79gw <hjh79gw@gmail.com>
2026-04-04 17:36:44 +0900

    fix(startGame): delete unused import (#65)

--- src/main/java/com/solv/wefin/domain/game/room/event/GameRoomEvent.java
+++ src/main/java/com/solv/wefin/domain/game/room/event/GameRoomEvent.java
@@ -1,8 +1,5 @@
 package com.solv.wefin.domain.game.room.event;
 
-import jdk.jfr.Event;
-import jdk.jfr.EventType;
-
 import java.util.UUID;
 
 public record GameRoomEvent(UUID roomId, EventType type) {
