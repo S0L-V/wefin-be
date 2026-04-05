06f18cfa69543114f3c06ebb899c0731f0dfa175
cl-o-lc <sujin2839@gmail.com>
2026-04-06 02:21:14 +0900

    fix(market): remove TradeResponse reference to fix build (#70)

--- src/main/java/com/solv/wefin/domain/trading/market/client/HantuWebSocketClient.java
+++ src/main/java/com/solv/wefin/domain/trading/market/client/HantuWebSocketClient.java
@@ -1,7 +1,6 @@
 package com.solv.wefin.domain.trading.market.client;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.solv.wefin.domain.trading.market.dto.TradeResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
