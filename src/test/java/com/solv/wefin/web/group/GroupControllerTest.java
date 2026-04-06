b4d7f3ccee1482c25a7c2334e919994d77a90a2b
kkhhmm3103 <kkhhmm23@gmail.com>
2026-04-06 14:08:20 +0900

    refactor(test): replace @MockBean with @MockitoBean (#120)

--- src/test/java/com/solv/wefin/web/group/GroupControllerTest.java
+++ src/test/java/com/solv/wefin/web/group/GroupControllerTest.java
@@ -8,9 +8,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.OffsetDateTime;
@@ -30,10 +30,10 @@ class GroupControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private GroupService groupService;
 
-    @MockBean
+    @MockitoBean
     private JwtProvider jwtProvider;
 
     @Test
